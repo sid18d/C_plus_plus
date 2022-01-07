6b445a091f9422c6de88ab1f455721d11273f360
Siddharth Dixit <sid18dixit@gmail.com>
2022-01-08 04:35:20 +0530

    Add files via upload

---
+++ hackerrank/intro2.cpp
@@ -0,0 +1,23 @@
+
+/* 
+Read 3 numbers from stdin and print their sum to stdout.
+
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
