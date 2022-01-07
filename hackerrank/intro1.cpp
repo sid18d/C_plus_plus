605ba2538122812446c017c3f59d41700f61c309
Siddharth Dixit <sid18dixit@gmail.com>
2022-01-08 04:34:59 +0530

    Add files via upload

---
+++ hackerrank/intro1.cpp
@@ -0,0 +1,14 @@
+
+/* 
+ Write a program in C++ to print a Hello World  
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+
