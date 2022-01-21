990d7fc529fdc34f35d8db6aa26511bf13c5f977
Siddharth Dixit <sid18dixit@gmail.com>
2022-01-21 18:59:15 +0530

    Add files via upload

---
+++ CodeChef/RichieRich.cpp
@@ -0,0 +1,23 @@
+/*
+CodeChef
+ Richie Rich , Problem Code: CHFRICH
+*/
+
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   int a,b,p;
+	   cin>>a>>b>>p;
+	   int q=b-a;
+	   cout<<q/p<<endl;
+	}
+	return 0;
+}
